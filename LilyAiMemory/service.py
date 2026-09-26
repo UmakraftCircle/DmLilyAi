51fe2c66ddac0ae8af7466c8f9271d52f6d0b74f
UmakraftCircle <pagaranjayson04@protonmail.com>
2026-09-26 22:23:08 +0800

    Expose DeficitStateStore on MemoryService (memory.deficit_state)

--- LilyAiMemory/service.py
+++ LilyAiMemory/service.py
@@ -1,6 +1,7 @@
 """Facade for the Memory domain."""
 from LilyAiCore.ExternalServices.Database.sqlite import Database
 from LilyAiMemory.ConversationMemory.conversation_memory import ConversationMemory
+from LilyAiMemory.DeficitState.deficit_state_store import DeficitStateStore
 from LilyAiMemory.FanGain.fan_gain import FanSnapshotStore
 from LilyAiMemory.Retrieval.retriever import rank_by_overlap
 from LilyAiMemory.SessionMemory.session_memory import SessionMemory
@@ -17,6 +18,7 @@ class MemoryService:
         self.session = SessionMemory()
         self.trainer_link = TrainerLinkStore(self.store)
         self.fan_gain = FanSnapshotStore(self.store)
+        self.deficit_state = DeficitStateStore(self.store)
 
     def relevant_user_facts(self, user_id: str, query: str, limit: int = 6) -> list[str]:
         """Most relevant facts for this message; falls back to the newest facts so basics aren't lost."""
